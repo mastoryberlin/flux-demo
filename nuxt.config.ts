a905ad6dab2897ea10ec02974df68656f1b2c339
Felix Schwarz <felix@mastory.io>
2025-10-08 14:19:36 +0200

    chore: upgrade nuxt+remove auth

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,24 +9,12 @@ export default defineNuxtConfig({
     '@nuxthub/core',
     '@nuxt/eslint',
     '@nuxt/ui',
-    '@kgierke/nuxt-basic-auth'
   ],
 
   // https://hub.nuxt.com/docs/getting-started/installation#options
   hub: {
     ai: true,
     blob: true,
-  },
-
-  basicAuth: {
-    enabled: process.env.NODE_ENV === 'production',
-    allowedRoutes: ['/api/_hub/'],
-    users: [
-      {
-        username: process.env.USERNAME || 'admin',
-        password: process.env.PASSWORD || 'admin'
-      }
-    ]
   },
 
   // https://eslint.nuxt.com
